789188c9b4cba25f3e384e19044adbd3863334e8
ShallowOatmeal <miguelmantilla1305@gmail.com>
2024-07-09 19:37:13 -0400

    Completed skeletal structure; added functions. Update 3

---
+++ javaScrpt.js
@@ -0,0 +1,27 @@
+
+function getComputerChoice() {
+    let info = "tbd";
+    console.log(info);
+}
+
+function getHumanChoice() {
+    let txt = prompt("Rock, Paper, or Scissors?");
+    console.log(txt);
+}
+
+
+let humanScore, computerScore = 0;
+console.log(humanScore)
+console.log(computerScore)
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = 'tbd';
+    computerChoice = 'tbd';
+}
+
+
+function playGame(){
+    "to be defined";
+}
+
