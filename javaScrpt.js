55b7996c5416d0dbd6d8e9ab52428c82f7513f5d
Shallow Oatmeal <miguelmantilla1305@gmail.com>
2024-07-22 20:23:00 -0400

    Completed project for Rock Paper Scissors Revisited! Looks pretty cool.

--- javaScrpt.js
+++ javaScrpt.js
@@ -28,6 +28,9 @@ humanDecision.addEventListener("click", (e) => {
         case 'scissors':
             humanChoice = "scissors";
             break;
+
+        default:
+            humanChoice = "ERROR";
         }
         if (humanChoice) {
             callback(humanChoice);
@@ -36,50 +39,53 @@ humanDecision.addEventListener("click", (e) => {
 }
 
 
-getHumanChoice();
-
-
-/*
-
 let humanScore = 0, computerScore = 0, rounds = 0;
 console.log(humanScore, computerScore, rounds);
 
+const scoreHeaderHuman = document.querySelector('#hooman-score');
+const scoreHeaderRobot = document.querySelector('#robot-score');
+const roundsHeader = document.querySelector('#round');
 
 function playRound(humanChoice, computerChoice) {
     console.log("Human Choice: " + humanChoice, "Computer Choice: " + computerChoice);
     if (humanChoice == computerChoice) {
-        console.log("There's no winner, it's a tie!");
+        alert("There's no winner for this round, it's a tie...");
     } else if (
         computerChoice === "rock" && humanChoice === "paper" ||
         computerChoice === "scissors" && humanChoice === "rock" ||
         computerChoice === "paper" && humanChoice === "scissors"
     ) {
-        console.log("Human wins! Good game!");
-        humanScore++;
+        humanScore++
+        scoreHeaderHuman.textContent = `Humanity Score: ${humanScore}`;
     } else {
-        console.log("Computer Wins! Great job Robot!")
         computerScore++;
+        scoreHeaderRobot.textContent = `Machines Score: ${computerScore}`;
     }
     rounds ++
-    console.log("Human Score: " + humanScore, "Computer Score: " + computerScore, "Rounds: " + rounds);
+    roundsHeader.textContent = `Rounds: ${rounds}`;
 }
 
-function playGame() {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        if (humanSelection !== '') {
-        playRound(humanSelection, computerSelection);
-    }
+
+function playNewGame(choice) {
+    const computerSelection = getComputerChoice();
+    const humanSelection = choice;
+    playRound(humanSelection, computerSelection);
+    if (rounds >= 5) {
+        if (humanScore > computerScore) {
+            alert("Human wins!! Congrats hooman.");
+        } else if (computerScore > humanScore) {
+            alert("Computer wins!! Congrats to our Machine Overlords.");
+        } else {
+            alert("It's a tie?!! How could this happen.");
+        }
+    humanScore = 0;
+    scoreHeaderHuman.textContent = `Humanity Score: ${humanScore}`;
+    computerScore = 0;
+    scoreHeaderRobot.textContent = `Machines Score: ${computerScore}`;
+    rounds = 0;
+    roundsHeader.textContent = `Rounds: ${rounds}`;
     }
 
-    if (humanScore > computerScore) {
-        alert("Human wins!! Congrats hooman.");
-    } else if (computerScore > humanScore) {
-        alert("Computer wins!! Congrats to our Machine Overlords.");
-    } else {
-        alert("It's a tie?!! How could this happen.");
-    }
+}
 
-playGame();
-
-*/
+getHumanChoice(playNewGame);
